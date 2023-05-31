61edd12e284a6fe426b4b45b45cbe89502888c5b
JUWONCALEB <63285430+juwoncaleb@users.noreply.github.com>
2023-05-31 19:48:24 +0100

    send

--- pages/connect.js
+++ pages/connect.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from '../components/Header'
 import Footer from '../components/Footer';
 
 export default function Connect() {
+    const [open, setOpen] = useState(1)
+
     return (
         <div>
             <Header />
@@ -16,18 +18,134 @@ export default function Connect() {
                 </div>
             </div>
             <div className='header'>
-                <div className='flex justify-around toggle_options_connect'>
-                    <p className='conntect_toggle'>All</p>
-                    <p className='conntect_toggle'>Trade association</p>
-                    <p className='conntect_toggle'>Retailer</p>
+                <div className='flex justify-around toggle_options_connect mb-8'>
+                    <div onClick={() => setOpen(1)} >
+                        <p className='conntect_toggle'>All</p>
+                        <hr className={open ===1 ? "green_line" : null} />
+                    </div>
+                    <div onClick={() => setOpen(2)}>
+                        <p className='conntect_toggle'>Trade association</p>
+                        <hr className={open ===2 ? "green_line" : null}/>
+
+                    </div>
+                    <div onClick={() => setOpen(3)}>
+                        <p  className='conntect_toggle'>Retailer</p>
+                        <hr className={open ===3 ? "green_line" : null} />
+                    </div>
                     <p className='conntect_toggle'>Ambassador</p>
                     <p className='conntect_toggle'>Training</p>
 
                 </div>
+                {/* ALL ITEMS */}
+                {
+                    open == 1 && (
+                        <div>
+                            <div className='flex justify-around desk_items'>
+                                <div className='desk_theme'>
+                                    <p className='desk_head'>Trade Associations</p>
+                                    <p> These companies or organization provides products or services that are related to, or complementary to, your our business. For examples, parts supply deals for our home service professionals.</p>
+                                </div>
+                                <img src='./desk.png' />
+                            </div>
+                            <div className='flex justify-around mt-10 desk_items'>
+                                <div className='desk_theme'>
+                                    <p className='desk_head'>Retailers</p>
+                                    <p>Retailer partners are companies or organization that sells your products or services to end customers.</p>
 
-                <div >
-                    <img src='./t1.png' />
-                </div>
+                                </div>
+                                <img src='./desk.png' />
+                            </div>
+                            <div className='flex justify-around mt-10 desk_items'>
+                                <div className='desk_theme'>
+                                    <p className='desk_head'>Ambassadors</p>
+                                    <p>Ambassadorial partners are typically individuals who love and want to represent our brand and promotes our products or services to others.</p>
+                                </div>
+                                <img src='./desk.png' />
+                            </div>
+                            <div className='flex justify-around mt-10 desk_items mb-20'>
+                                <div className='desk_theme'>
+                                    <p className='desk_head'>Training</p>
+                                    <p>Training partners are companies or organization that provides training or educational resources related to our business.</p>
+                                </div>
+                                <img src='./desk.png' />
+                            </div>
+                        </div>
+                    )
+                }
+
+
+                {/* TRADE ASSOCIAION */}
+                {
+                    open === 2 && (
+                        <div>
+                            <div className='flex justify-around desk_items'>
+                                <div className='desk_theme'>
+                                    <p className='desk_head'>Trade Associations</p>
+                                    <p> These companies or organization provides products or services that are related to, or complementary to, your our business. For examples, parts supply deals for our home service professionals.</p>
+                                </div>
+                                <img src='./desk.png' />
+                            </div>
+                            <div className='mt-10'>
+                                <p className='desk_head_trade'>Our trade partners</p>
+                                <div className='flex justify-around'>
+                                    <div>
+                                        <img src='./Ziprecruiter.png' />
+                                        <p className='trad_path'>Zip recruiter</p>
+                                    </div>
+                                    <div>
+                                        <img src='./rangers.png' />
+                                        <p className='trad_path'>New York Rangers</p>
+                                    </div>
+                                    <div>
+                                        <img src='./Confluence.png' />
+                                        <p className='trad_path'>Confluence</p>
+                                    </div>
+                                    <div>
+                                        <img src='./iron_mon.png' />
+                                        <p className='trad_path'>Iron man</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                }
+
+                {/* TRADE ASSOCIAION */}
+                {
+                    open === 3 && (
+                        <div>
+                            <div className='flex justify-around mt-10 desk_items'>
+                                <div className='desk_theme'>
+                                    <p className='desk_head'>Retailers</p>
+                                    <p>Retailer partners are companies or organization that sells your products or services to end customers.</p>
+
+                                </div>
+                                <img src='./desk.png' />
+                            </div>
+                            <div className='mt-10'>
+                                <p className='desk_head_trade'>Our trade partners</p>
+                                <div className='flex justify-around'>
+                                    <div>
+                                        <img src='./Ikea.png' />
+                                        <p className='trad_path'>IKEA</p>
+                                    </div>
+                                    <div>
+                                        <img src='./punch.png' />
+                                        <p className='trad_path'>Hawaiian punch</p>
+                                    </div>
+                                    <div>
+                                        <img src='./Jeep.png' />
+                                        <p className='trad_path'>Jeep</p>
+                                    </div>
+                                    <div>
+                                        <img src='./Gusto.png' />
+                                        <p className='trad_path'>Gusto</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                }
             </div>
             <Footer />
 
---
+++ pages/earn.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import Header from '../components/Header'
+import Footer from '../components/Footer';
+export default function Earn() {
+    return (
+        <div>
+            <Header />
+            <div className='earn_bg mt-4'>
+                <div className='consumer_work_text'>
+                    <p className='connect_head'>Earn money by inviting <br /> someone to Loggworks</p>
+                    <p className='connect_Sub mt-6'>Loggworks connect homeowners to genuine and reliable service providers  <br /> who provide easy-to-book and hassle-free services.</p>
+                    <button className='cons_button'>
+                        <p>Connect us</p>
+                    </button>
+                </div>
+            </div>
+            <div className='header'>
+                <p className='information mb-10'>How it works</p>
+                <div className='grid grid-cols-3 gap-6'>
+                    <div className='afil_card'>
+                        <img src='./Privacy.png' />
+                        <p className='affiate'>Join Loggworks as an affiliate</p>
+                        <p> Leave the hard work and expense of web development and SEO to Loggworks. Our app also eliminates accounting costs by automating invoicing and reporting tasks.
+                        </p>
+                    </div>
+
+                    <div className='afil_card'>
+                        <img src='./cent.png' />
+                        <p className='affiate'> Share your affiliate link</p>
+                        <p>Cut the cost of admin by paying a commission only on completed bookings. The Loggworks
+                            pay-as-you-go model includes free registration, free offer promotion and free business reputation vetting — allowing you to scale as you grow.</p>
+                    </div>
+
+                    <div className='afil_card'>
+                        <img src='./box.png' />
+                        <p className='affiate'>Start earning when your link coverts</p>
+                        <p>Find new jobs and grow your business by using in-built features such as consumer financing,
+                            professional profile social sharing, vetting and special offers.</p>
+                    </div>
+                </div>
+
+                <div className='flex mt-10 justify-between mb-20'>
+                    <div className='handy_text'>
+                        <p className='information '>Perks & Benefit</p>
+                        <div className='info_head mt-10'>
+                            <p className='info_text_head mb-4'>Add to your earning</p>
+                            <p>A salary reflective of your skills and experience, based of international standards.</p>
+                        </div>
+                        <div className='info_head mt-10'>
+                            <p className='info_text_head mb-4'>Professional support</p>
+                            <p>Health insurance benefits to all team members, which cover medical, dental, and vision care.</p>
+                        </div>
+                        <div className='info_head mt-10'>
+                            <p className='info_text_head mb-4'>Coupons and discounts</p>
+                            <p>Paid time off for vacation, sick days, and holidays.</p>
+                        </div>
+                        <button className='cons_button'>
+                            <p>Become an Affliate</p>
+                        </button>
+                    </div>
+                    <img src='./handy.png' />
+
+                </div>
+            </div>
+            <Footer />
+        </div>
+    )
+}
