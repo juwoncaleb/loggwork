5247cc824d2ac5e4fd871269ac5aa22a47fd5191
JUWONCALEB <63285430+juwoncaleb@users.noreply.github.com>
2023-06-14 17:28:46 +0100

    send form

--- pages/message.js
+++ pages/message.js
@@ -42,6 +42,86 @@ export default function Message() {
   console.log(images)
   console.log(files);
 
+  const [currentForm, setCurrentForm] = useState(0);
+
+  const handleNextForm = () => {
+    setCurrentForm(currentForm + 1);
+  };
+
+  const handlePreviousForm = () => {
+    setCurrentForm(currentForm - 1);
+  };
+
+  const form = [
+    <div className=''>
+      <div>
+        <p className='form_text'>Title</p>
+        <input className='message_input mb-8' />
+        <p className='form_text'>Job Description</p>
+        <input required className='message_input_des' />
+        <div onClick={toggleModal} className='message_input mb-8 flex justify-between' >
+          <p className='mt-3 ml-2 form_text'>Category</p>
+          <img className='cart_arrow mt-4 mr-2' width="50" height="50" src="https://img.icons8.com/ios/50/less-than.png" alt="less-than" />
+        </div>
+        <div onClick={() => { setStage(2); setLocation(2) }} className='job_btn'>
+          <p className='form_text'>Enter location & Date</p>
+        </div>
+      </div>
+    </div>,
+    <div className=''>
+      <p className='form_text'>Location</p>
+      <input placeholder='  Enter postcode' className='message_input mb-2' />
+      <p className='address_shade_text mb-8'>Street address , Postcode , P.O. Box </p>
+      <div onClick={() => { setStage(3); setBudget(2) }} className='location_button'>
+        <p className='form_text'>Enter a Budget</p>
+      </div>
+    </div>,
+    <div className=''>
+      <div className='grid grid-cols-2 gap-4'>
+        <p className='form_text'>From</p>
+        <p className='form_text'>To</p>
+
+        <input placeholder='  $' className='message_input mb-2' />
+        <input placeholder='  $' className='message_input mb-2' />
+      </div>
+
+      <div onClick={() => { setStage(4); setZip(2) }} className='job_btn mt-4'>
+        <p className='form_text'>Upload Images</p>
+      </div>
+    </div>,
+    <div className=''>
+      <div>
+        <p className='form_text'>Upload an image (Optional)</p>
+
+        <div className='grid md:grid-cols-4 grid-cols-2 mt-6 '>
+          <div className='image_upload mt-2' {...getRootProps()}>
+            <p>+</p>
+            <input {...getInputProps()} />
+          </div>
+
+          <div className='image_upload mt-2 ' {...getRootProps()}>
+            <p>+</p>
+            <input {...getInputProps()} />
+          </div>
+
+          <div className='image_upload mt-2' {...getRootProps()}>
+            <p>+</p>
+            <input {...getInputProps()} />
+          </div>
+
+          <div className='image_upload mt-2' {...getRootProps()}>
+            <p>+</p>
+            <input {...getInputProps()} />
+          </div>
+        </div>
+        <p className='upload_image_texts'>Add supporting images to best showcase your job request</p>
+      </div>
+      <div onClick={uploadImages} className='job_btn mt-4'>
+        <p className='form_text'>Post Job</p>
+      </div>
+    </div>
+  ]
+
   return (
     <div className={isModalOpen === true ? "grayBg" : null}>
       <div className='header flex justify-left message_header'>
@@ -50,18 +130,21 @@ export default function Message() {
       <div className='header message_content'>
         <div>
           <div className='flex'>
-            <div className='back_arrow_div'>
+            <div onClick={handlePreviousForm} className='back_arrow_div'>
               <img className='back_arrow' width="50" height="50" src="https://img.icons8.com/ios/50/less-than.png" alt="less-than" />
+            </div>
+            <div onClick={handleNextForm} className='back_arrow_div ml-4'>
+              <img className='forward_arrow' width="50" height="50" src="https://img.icons8.com/ios/50/less-than.png" alt="less-than" />
             </div>
             <p className='post_header'>Post a Job</p>
           </div>
-          <div className='flex'>
-            <div className=''>
+          <div className='md:flex grid'>
+            <div className=' md:grid flex justify-around'>
               <div className='flex'>
                 <div>
 
                 </div>
-                <p className='stages_text_norm mt-2'>Job details</p>
+                <p className='stages_text_norm md:mt-2 mt-8'>Job details</p>
               </div>
               <div className='flex mt-8'>
 
@@ -72,200 +155,15 @@ export default function Message() {
                 <p className={budget === 2 ? "stages_text_norm" : "stages_text"}>Budget</p>
               </div>
               <div className='flex mt-8'>
-                
-              <p className={zip === 2 ? "stages_text_norm" : "stages_text"}>Upload Images</p>
+
+                <p className={zip === 2 ? "stages_text_norm" : "stages_text"}>Upload Images</p>
               </div>
             </div>
 
 
-            <div className='message_form'>
+            <div className='message_form md:mt-0 mt-6'>
 
-              {/* JOB DETAILS */}
-
-              {
-                stages === 1 && (
-                  <div>
-                    <p className='form_text'>Title</p>
-                    <input className='message_input mb-8' />
-                    <p className='form_text'>Job Description</p>
-                    <input required className='message_input_des' />
-                    <div onClick={toggleModal} className='message_input mb-8 flex justify-between' >
-                      <p className='mt-3 ml-2 form_text'>Category</p>
-                      <img className='cart_arrow mt-4 mr-2' width="50" height="50" src="https://img.icons8.com/ios/50/less-than.png" alt="less-than" />
-                    </div>
-                    <div onClick={() => { setStage(2); setLocation(2) }} className='job_btn'>
-                      <p className='form_text'>Enter location & Date</p>
-                    </div>
-                  </div>
-                )
-              }
-
-              {/* LOCATION */}
-
-              {
-                stages === 2 && (
-                  <div>
-                    <p className='form_text'>Location</p>
-                    <input placeholder='  Enter postcode' className='message_input mb-2' />
-                    <p className='address_shade_text mb-8'>Street address , Postcode , P.O. Box </p>
-                    <div onClick={() => { setStage(3); setBudget(2) }} className='location_button'>
-                      <p className='form_text'>Enter a Budget</p>
-                    </div>
-                  </div>
-                )
-              }
-              {/* BUDGET */}
-              {
-                stages === 3 && (
-                  <div>
-                    <div className='grid grid-cols-2 gap-4'>
-                      <p className='form_text'>From</p>
-                      <p className='form_text'>To</p>
-
-                      <input placeholder='  $' className='message_input mb-2' />
-                      <input placeholder='  $' className='message_input mb-2' />
-                    </div>
-
-                    <div onClick={() => { setStage(4) ; setZip(2) }} className='job_btn mt-4'>
-                      <p className='form_text'>Upload Images</p>
-                    </div>
-                  </div>
-                )
-              }
-
-
-              {/* UPLOAD IMAGES */}
-              {
-                stages === 4 && (
-                  <div>
-                    <div>
-                      <p className='form_text'>Upload an image (Optional)</p>
-
-                      <div className='grid md:grid-cols-4 grid-cols-2 mt-6 '>
-                        <div className='image_upload' {...getRootProps()}>
-                          <p>+</p>
-                          <input {...getInputProps()} />
-                        </div>
-
-                        <div className='image_upload ' {...getRootProps()}>
-                          <p>+</p>
-                          <input {...getInputProps()} />
-                        </div>
-
-                        <div className='image_upload' {...getRootProps()}>
-                          <p>+</p>
-                          <input {...getInputProps()} />
-                        </div>
-
-                        <div className='image_upload' {...getRootProps()}>
-                          <p>+</p>
-                          <input {...getInputProps()} />
-                        </div>
-                      </div>
-                      <p className='upload_image_texts'>Add supporting images to best showcase your job request</p>
-                    </div>
-                    <div onClick={uploadImages} className='job_btn mt-4'>
-                      <p className='form_text'>Post Job</p>
-                    </div>
-                  </div>
-
-                )
-              }
-              {/* MODAL */}
-              {isModalOpen && (
-                <div className='modal'>
-                  <div className='flex justify-between'>
-                    <p className='modal_head'>Categories</p>
-                    <p className='modal_head_minus cursor-pointer' onClick={toggleModal}>x</p>
-                  </div>
-                  <input placeholder=' Search' className='message_input mb-8' />
-                  <div className='flex justify-between'>
-                    <div classN className='flex' ame='flex'>
-                      <img className="local_icon" src='../../Icons/vector.png' />
-                      <p className='cartegory_texts'>Catering</p>
-                    </div>
-                    <img className='cart_arrow mt-4 mr-2' width="50" height="50" src="https://img.icons8.com/ios/50/less-than.png" alt="less-than" />
-
-                  </div>
-
-                  <div className='flex justify-between'>
-                    <div className='flex'>
-                      <img className="local_icon" src='../../Icons/vector-1.png' />
-                      <p className='cartegory_texts'>Childcare</p>
-                    </div>
-                    <img className='cart_arrow mt-4 mr-2' width="50" height="50" src="https://img.icons8.com/ios/50/less-than.png" alt="less-than" />
-                  </div>
-
-                  <div className='flex justify-between'>
-                    <div className='flex'>
-                      <img className="local_icon" src='../../Icons/vector-2.png' />
-                      <p className='cartegory_texts'>Cleaning</p>
-                    </div>
-                    <img className='cart_arrow mt-4 mr-2' width="50" height="50" src="https://img.icons8.com/ios/50/less-than.png" alt="less-than" />
-                  </div>
-
-                  <div className='flex justify-between'>
-                    <div className='flex'>
-                      <img className="local_icon" src='../../Icons/vector-3.png' />
-                      <p className='cartegory_texts'>Entertainment</p>
-                    </div>
-                    <img className='cart_arrow mt-4 mr-2' width="50" height="50" src="https://img.icons8.com/ios/50/less-than.png" alt="less-than" />
-                  </div>
-                  <div className=''>
-                    <div className='flex justify-between'>
-                      <div className='flex'>
-                        <img className="local_icon" src='../../Icons/vector-4.png' />
-                        <p className='cartegory_texts'>Appliance Repair</p>
-                      </div>
-                      <img onClick={() => isOpen(open === 1 ? null : 1)} className='cart_arrow mt-4 mr-2' width="50" height="50" src="https://img.icons8.com/ios/50/less-than.png" alt="less-than" />
-
-                    </div>
-                    {
-                      open === 1 && (
-                        <div className='ml-12'>
-                          <div className='flex'>
-                            <div className='outer_ring mr-4'>
-                              <div className='inner_ring'>
-
-                              </div>
-                            </div>
-                            <p>Domestic appliance repair</p>
-                          </div>
-                          <div className='flex mb-6 mt-2'>
-                            <div className='outer_ring mr-4'>
-                              <div className='inner_ring'>
-
-                              </div>
-                            </div>
-                            <p>Electronic repair</p>
-                          </div>
-                        </div>
-                      )
-                    }
-                  </div>
-                  <div className='flex justify-between'>
-                    <div className='flex'>
-                      <img className="local_icon" src='../../Icons/vector-5.png' />
-                      <p className='cartegory_texts'>Mobile Mechanic</p>
-                    </div>
-                    <img className='cart_arrow mt-4 mr-2' width="50" height="50" src="https://img.icons8.com/ios/50/less-than.png" alt="less-than" />
-                  </div>
-                  <div className='flex justify-between'>
-                    <div className='flex'>
-                      <img className="local_icon" src='../../vector.png' />
-                      <p className='cartegory_texts'>Pet Groomer</p>
-                    </div>
-                    <img className='cart_arrow mt-4 mr-2' width="50" height="50" src="https://img.icons8.com/ios/50/less-than.png" alt="less-than" />
-                  </div>
-                  <div className='flex justify-between'>
-                    <div className='flex'>
-                      <img className="local_icon" src='../../Icons/vector-9.png' />
-                      <p className='cartegory_texts'>Computer <br /> Phone repair</p>
-                    </div>
-                    <img className='cart_arrow mt-4 mr-2' width="50" height="50" src="https://img.icons8.com/ios/50/less-than.png" alt="less-than" />
-                  </div>
-                </div>
-              )}
+            {form[currentForm]}
             </div>
 
 
