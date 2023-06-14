e2c04ac6d3b3bb7a2a1a146330e15def90486a1d
JUWONCALEB <63285430+juwoncaleb@users.noreply.github.com>
2023-06-14 14:38:00 +0100

    git sucess

---
+++ pages/professional_user/auth/reset_success.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function Reset_success() {
+    return (
+        <div className='reset_div'>
+            <center>
+                <img className='logo mb-24' src='https://ik.imagekit.io/juwoncaleb/lo_wCz74a4lV.webp?updatedAt=1685112934249' />
+
+                <div className='reset_form mt-10'>
+
+                    <img className='mt-10' src='../../emaile.png' />
+                    <p className='pro_signin_cta_header'>Password reset email sent.</p>
+                    <p>A reset link has been sent to your mailbox. Check and click the link to reset password.</p>
+                    <p className='new_accounr'>Still have issues resetting your password? <span className='no_account support_link'>Contact Support</span></p>
+                    <p className='mt-8 back_login mb-10'>Back to login</p>
+
+                </div>
+            </center>
+
+        </div>
+    )
+}
