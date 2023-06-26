ccb3c709c9cd262e66d414e59546994f26872df8
JUWONCALEB <63285430+juwoncaleb@users.noreply.github.com>
2023-06-26 16:02:45 +0100

    update side

--- pages/professional_user/components/sidebar.js
+++ pages/professional_user/components/sidebar.js
@@ -9,24 +9,24 @@ export default function Sidebar() {
         <p>Home</p>
       </div>
       <div className='flex'>
-        <img width="25" height="25" src="https://img.icons8.com/external-inkubators-glyph-inkubators/25/1A1A1A/external-briefcase-business-inkubators-glyph-inkubators.png" alt="external-briefcase-business-inkubators-glyph-inkubators" />
+        <img width="30" height="25" src="https://img.icons8.com/external-inkubators-glyph-inkubators/25/1A1A1A/external-briefcase-business-inkubators-glyph-inkubators.png" alt="external-briefcase-business-inkubators-glyph-inkubators" />
         <p>Jobs</p>
       </div>
       <div className='flex'>
-        <img src='./arc.pmg' />
+      <img width="30" height="50" src="https://img.icons8.com/ios-filled/50/list.png" alt="list"/>
         <p>Activities</p>
 
       </div>
       <div className='flex'>
-        <img width="50" height="50" src="https://img.icons8.com/ios/50/wallet--v1.png" alt="wallet--v1" />
+        <img width="30" height="50" src="https://img.icons8.com/ios/50/wallet--v1.png" alt="wallet--v1" />
         <p>Wallet</p>
       </div>
       <div className='flex'>
-      <img width="50" height="50" src="https://img.icons8.com/ios/50/speech-bubble-with-dots--v1.png" alt="speech-bubble-with-dots--v1"/>
+      <img width="30" height="50" src="https://img.icons8.com/ios/50/speech-bubble-with-dots--v1.png" alt="speech-bubble-with-dots--v1"/>
         <p>Message</p>
       </div>
       <div>
-      <img width="50" height="50" src="https://img.icons8.com/ios/50/name--v1.png" alt="name--v1"/>
+      <img width="30" height="50" src="https://img.icons8.com/ios/50/name--v1.png" alt="name--v1"/>
         <p>Profile</p>
       </div>
     </div>
